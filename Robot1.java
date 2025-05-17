beb5a8f9dbf7728dda30b34438f9f75c8d3d664f
pazu0000 <ke0519nshi@yahoo.co.jp>
2025-05-17 17:59:18 +0900

    Robot1クラス

---
+++ Robot1.java
@@ -0,0 +1,15 @@
+class Robot1{
+  public static void main(String[] args){
+    Robot1 robot = new Robot1();
+    robot.hello();
+    robot.bye();
+  }
+
+  void hello(){
+    System.out.println("Hello");
+  }
+
+  void bye(){
+    System.out.println("Bye");
+  }
+}
