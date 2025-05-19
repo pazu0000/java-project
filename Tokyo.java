3bd360aae3ba1250ffea61d428971134e63d05fd
pazu0000 <ke0519nshi@yahoo.co.jp>
2025-05-19 09:58:34 +0900

    Go東京!!

---
+++ Tokyo.java
@@ -0,0 +1,13 @@
+public class Tokyo {
+  public static void main(String[] args){
+    Person person1 = new Person();
+    person1.name = "Kenshi Fukuda";
+    person1.hello();
+    person1.goToTokyo();
+
+    Person person2 = new Person();
+    person2.name = "Motozaki";
+    person2.hello();
+    person2.goToTokyo();
+  }
+}
